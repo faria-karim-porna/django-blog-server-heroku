ceb70dc47e803cc113568feef4fa97fe414b4fe6
faria-karim-porna <faria.porna.08@gmail.com>
2020-12-29 09:03:17 +0600

    first commit

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
